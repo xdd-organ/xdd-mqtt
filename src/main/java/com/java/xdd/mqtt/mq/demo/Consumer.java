02137ebbf2a10a9f3d4938d739f1c85a68d73c55
xdd <767403510@qq.com>
2018-09-10 18:12:44 +0800

    mq指定消费者完成

--- src/main/java/com/java/xdd/mqtt/mq/demo/Consumer.java
+++ src/main/java/com/java/xdd/mqtt/mq/demo/Consumer.java
@@ -12,7 +12,7 @@ public class Consumer {
 	}
 
 	// 使用JmsListener配置消费者监听的队列，其中text是接收到的消息
-	@JmsListener(destination = "mytest.queue", selector = "systemcode=aaaa")//接收指定的消费
+	@JmsListener(destination = "mytest.queue", selector = "systemcode='aaaa'")//接收指定的消费
 	public void receiveQueue2(String text) {
 		System.out.println("Consumer2收到的报文为:"+text);
 	}
